9663064115ff528527754d724cd9a657422d8d88
P24aelsheikh <144148097+P24aelsheikh@users.noreply.github.com>
2023-10-17 09:12:07 -0400

    Add files via upload

---
+++ lab_4-4.py
@@ -0,0 +1,30 @@
+"""
+
+*** You must write a comment for every chunk of code you write from now on along with your author tag***
+
+Create a Python file named lab_4-4.py
+
+Use the find method to return the index of the first occurrence of the letter "t" in the word "flibbertigibbet".
+
+Using this value, print the letter immediately following the first "t". hint: you may need to store the index value as a variable!
+
+Create a variable storing you first name written in lowercase. Using a string method, print this variable in uppercase.
+
+Use the split method to split the following sentence at each comma: "I wish, I wish, I was a fish."
+
+"""
+x= 'flibbertigibbet'
+b=x[7:8:]
+print(b)
+c=x[8:9:]
+print(c)
+d='alsir'
+z=str.capitalize(d)
+print(z)
+g='I wish, I wish, I was a fish.'
+ad=g[0:8]
+print(ad)
+bd=g[8:16]
+print(bd)
+gc=g[16:]
+print(gc)
